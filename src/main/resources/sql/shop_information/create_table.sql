e05a9d4dd95eb2f272099216f4a2dc84e3d41093
zkingboos <kingboosoficial@gmail.com>
2020-10-19 19:39:25 -0300

    changed location to varchar(150)

--- src/main/resources/sql/shop_information/create_table.sql
+++ src/main/resources/sql/shop_information/create_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS shop_information (
     id int NOT NULL AUTO_INCREMENT,
     owner varchar(16) NOT NULL,
-    location varchar() DEFAULT NULL,
+    location varchar(150) DEFAULT NULL,
     description text DEFAULT NULL,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     KEY id(id),
